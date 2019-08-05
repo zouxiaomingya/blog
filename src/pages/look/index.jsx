ed96839073821e00f4d6ccf9ad15035b0e0f5965
zouxiaomingya <13870939865@163.com>
2019-08-06 00:28:47 +0800

    [feat]add_react-monaco-editor

--- src/pages/look/index.jsx
+++ src/pages/look/index.jsx
@@ -1,8 +1,30 @@
-import React from 'react';
-
-function Look () {
+import React, { Fragment } from 'react';
+// import MonacoEditor from 'react-monaco-editor';
+import { code } from './code'
+console.log(code);
+function Look() {
+  const editorDidMount = (editor, monaco) => {
+    console.log('editorDidMount', editor);
+    editor.focus();
+  }
+  const onChange = (newValue, e) => {
+    console.log('onChange', newValue, e);
+  }
   return (
-    <div>12</div>
+    <Fragment>
+      {/* <MonacoEditor
+        width="800"
+        height="600"
+        language="javascript"
+        theme="vs-dark"
+        value={code}
+        options={options}
+        onChange={onChange}
+        editorDidMount={editorDidMount}
+      /> */}
+      <div>111</div>
+    </Fragment>
+
   )
 }
 export default Look;
