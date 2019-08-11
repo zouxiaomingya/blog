66c992b2c194f1bf79c60c8d16ecf2f75399a6da
zouxiaomingya <13870939865@163.com>
2019-08-11 19:33:15 +0800

    [fix]

---
+++ src/pages/read/ModalWrap.jsx
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+function ModalWrap({ children }) {
+  const [visible, setVisible] = useState(false);
+  const onOpen = () => setVisible(true);
+  const onClose = () => setVisible(false);
+  return children({ visible, onClose, onOpen })
+}
+export default ModalWrap;
--- src/pages/read/index.jsx
+++ src/pages/read/index.jsx
@@ -1,8 +1,25 @@
-import React from 'react';
-
-function Read () {
+import React, { Fragment } from 'react';
+import { Modal, Button} from 'antd';
+import ModalWrap from './ModalWrap'
+function Read() {
   return (
-    <div>Read</div>
+    <ModalWrap>
+      {({ visible, onClose, onOpen }) => {
+        return (
+          <Fragment>
+            <Button onClick={onOpen}>打开表单</Button>
+            <Modal
+              title="Basic Modal"
+              visible={visible}
+              // onOk={this.handleOk}
+              onCancel={onClose}
+            >
+              <p>sdd</p>
+            </Modal>
+          </Fragment>
+        )
+      }}
+    </ModalWrap>
   )
 }
 export default Read;
