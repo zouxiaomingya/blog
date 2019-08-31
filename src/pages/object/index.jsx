814bcf838511423b0a3a481373f9b314a11963b7
zouxiaomingya <13870939865@163.com>
2019-09-01 01:43:20 +0800

    [fix]

--- src/pages/object/index.jsx
+++ src/pages/object/index.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
-
-
-function Obj () {
-  return <div>1</div>
+const getSum = function (sign) {
+  if (sign === 'a') {
+    return (function () {
+      let sum = 0;
+      for (let i = 0; i < 1000000; i++) {
+        sum += i
+      }
+      return sum
+    })()
+  } else if (sign === 'b') {
+    return (function () {
+      let sum = 0;
+      for (let i = 0; i < 10000000000; i++) {
+        sum += i
+      }
+      return sum
+    })()
+  }
+}
+function Obj() {
+  return <div>{getSum('b')}</div>
 }
 
 export default Obj;
