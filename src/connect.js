fb555ef35f3e07f779037f3c257b9c50676bf1de
zouxiaomingya <13870939865@163.com>
2019-11-25 19:14:56 +0800

    feat:use useContext

--- src/connect.js
+++ src/connect.js
@@ -1,7 +1,8 @@
-import React, { createContext } from "react";
+import React, { createContext, useContext } from "react";
 import { useImmer } from "use-immer";
 
-const { Consumer, Provider } = createContext();
+const ctx = createContext();
+const { Consumer, Provider } = ctx;
 
 const useModel = initialState => {
   const [state, setState] = useImmer(initialState);
@@ -17,14 +18,20 @@ const createProvide = () => {
   return WrapProvider;
 };
 
-export const connect = fn => ComponentUi => () => (
-  <Consumer>
-    {state => {
-      const { _state, _dispatch } = state
-      const filterState = typeof fn === "function" ? fn(_state) : _state;
-      return <ComponentUi _state={filterState} _dispatch={_dispatch} />;
-    }}
-  </Consumer>
-);
+// export const connect = fn => ComponentUi => () => (
+//   <Consumer>
+//     {state => {
+//       const { _state, _dispatch } = state
+//       const filterState = typeof fn === "function" ? fn(_state) : _state;
+//       return <ComponentUi _state={filterState} _dispatch={_dispatch} />;
+//     }}
+//   </Consumer>
+// );
+
+export const connect = fn => ComponentUi => () => {
+  const { _state, _dispatch } = useContext(ctx);
+  const filterState = typeof fn === "function" ? fn(_state) : _state;
+  return <ComponentUi _state={filterState} _dispatch={_dispatch} />;
+};
 
 export default createProvide;
