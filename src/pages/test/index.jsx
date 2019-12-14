336ac787e18b7d4b9a6fc068abb704ca598da8fd
zouxiaomingya <13870939865@163.com>
2019-12-14 17:31:10 +0800

    feat: test function componment

--- src/pages/test/index.jsx
+++ src/pages/test/index.jsx
@@ -1,15 +1,45 @@
-import React, { useState, useEffect } from 'react';
-import Monaco from '../../common/Monaco';
-import { code } from './code'
+import React, { useState, useEffect } from "react";
+import { Input } from "antd";
+import Monaco from "../../common/Monaco";
+import { code } from "./code";
+
+const RenderOutInput = () => {
+  const [value, setValue] = useState("");
+  return (
+    <Input
+      value={value}
+      onChange={e => {
+        setValue(e.target.value);
+      }}
+    />
+  );
+};
 
 function App() {
   const [count, setCount] = useState(1);
+  const [value, setValue] = useState("");
   let name, setName, age, setAge;
   const handleAdd = () => {
-    setCount(count + 1)
+    setCount(count + 1);
+  };
+  const RenderInput = () => {
+    return (
+      <Input
+        value={value}
+        onChange={e => {
+          setValue(e.target.value);
+        }}
+      />
+    );
   };
   return (
     <div>
+      函数调用{RenderInput()}
+      组件调用
+      <RenderInput />
+      函数外的函数调用{RenderOutInput()}
+      函数内的函数调用
+      <RenderOutInput />
       <Monaco code={code} />
       <p>姓名: {name}</p>
       <p>年龄: {age}</p>
